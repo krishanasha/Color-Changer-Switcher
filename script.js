a243a5ac65952f176f919a3a198cbebfe68a3993
krishanasha <krishanasharma63771@gmail.com>
2025-01-16 13:30:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const buttons= document.querySelectorAll('.button')
+const body= document.querySelector("body")
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        // console.log(e.id);
+        // if(e.target.id === 'grey' ){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'white'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'blue'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'yellow'){
+        //     body.style.backgroundColor= e.target.id
+        // }
+
+        switch(e.target.id){
+            case 'grey':
+                body.style.backgroundColor = e.target.id;
+            case 'white':
+                body.style.backgroundColor = e.target.id;
+            case 'blue':
+                body.style.backgroundColor = e.target.id;
+            case 'yellow':
+                body.style.backgroundColor = e.target.id     
+        }
+    })
+});
